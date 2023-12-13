86ddca3e0cc99c492e75a59567d401a9e9e4b0b0
MIMBOL228 <aekemerov@yandex.ru>
2023-12-13 23:27:23 +0500

    Добавил текст для меню уведомлений

--- src/actions/notifyMenuAction.ts
+++ src/actions/notifyMenuAction.ts
@@ -3,5 +3,6 @@ import {notifyMenu} from "../langs/notifyMenu.js";
 import {notifyKeyboard} from "../keyboards/notifyKeyboard.js";
 
 export const notifyMenuAction: Action = async (ctx) => {
+    await ctx.answerCbQuery();
     await ctx.reply(notifyMenu(ctx.user.enabledNotify), notifyKeyboard(ctx.user.enabledNotify))
 }

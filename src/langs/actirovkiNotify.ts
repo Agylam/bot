b5489c551ed31ef27e3ad416cc96528a4b2efa25
MIMBOL228 <aekemerov@yandex.ru>
2024-01-11 21:47:04 +0500

    Запутался

--- src/langs/actirovkiNotify.ts
+++ src/langs/actirovkiNotify.ts
@@ -6,8 +6,8 @@ export const actirovkiNotify = (actirovkiStatus : UgraActirovkiStatus, user: Use
     if(actirovkiStatus.status === undefined){
         return `💔 К сожалению, информации по актировке для твоей смены сейчас нет...`;
     }else if(actirovkiStatus.status && actirovkiStatus.class_range !== undefined){
-        result = actirovkiStatus.class_range.level >= user.classRange? '❄️': '☀️' ; // Ура, актировка
-        result += ` Актировка с 1 по ${actirovkiStatus.class_range.to} класс\nЗанятия ${user.shift}й смены отменяются в связи с погодными условиями:`;
+        //result = actirovkiStatus.class_range.level >= user.classRange? '': '☀️' ; // Ура, актировка
+        result += `❄️ Актировка с 1 по ${actirovkiStatus.class_range.to} класс\nЗанятия ${user.shift}й смены отменяются в связи с погодными условиями:`;
     }else{
         result = `☀️ Актировки нет!\nВсе классы учатся в штатном режиме`; // Блин, актировки нет...
     }
